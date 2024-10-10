9bc1a1961e1c7bee36253f6ed85f13bbd7014e3d
tequila01-svg <onifadeayobami008@gmail.com>
2024-10-10 09:09:10 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# take input from user
+x=input("enter value of x:")
+y=input("enter value of y:")
+
+#swapping
+temp=x
+x=y
+y=temp
+
+#displaying results after swapping
+print("value of x after swapping:", x)
+print("value of y after swapping:", y)
